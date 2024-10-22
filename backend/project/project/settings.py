f47b6b3a63827d305895754a1bfc873155bb178c
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-10-23 07:41:07 +0800

    added header cors

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Add your frontend URL
+    # You can add other allowed origins as needed
+]
